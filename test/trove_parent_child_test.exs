c07536fbc0ad8309c952bec5a6549d080eb94842
Tad Scritchfield <tadscritch@gmail.com>
2022-09-19 16:37:53 -0500

    add test to expect empty result

--- test/trove_parent_child_test.exs
+++ test/trove_parent_child_test.exs
@@ -32,4 +32,31 @@ defmodule TroveParentChildTest do
 
     assert p.first_name == "Tad"
   end
+
+  test "Person query - could not find by child vehicle" do
+    {:ok, person1} = Repo.insert(%Person{first_name: "Tad", last_name: "S", age: 26})
+    {:ok, person2} = Repo.insert(%Person{first_name: "E", last_name: "S", age: 26})
+
+    {:ok, vehicle} =
+      Repo.insert(%Vehicle{
+        person: person1.id,
+        make: "Rivian",
+        model: "R1",
+        year: 2022,
+        color: "Blue",
+        license_plate: "123456",
+        date_registered: DateTime.utc_now() |> DateTime.truncate(:second)
+      })
+
+    records =
+      Person
+      |> Trove.search(%{
+        vehicle: %{
+          make: "Toyota"
+        }
+      })
+      |> Repo.all()
+
+    refute Enum.any?(records)
+  end
 end
